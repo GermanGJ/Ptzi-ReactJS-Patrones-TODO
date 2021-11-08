3b8491c0476117cf7603c03a2646c252146e2a9f
Server2GJ <GermanGomezJimenez@gmail.com>
2021-11-08 00:42:15 -0500

    Clase 7: Composicion sin Context

--- src/App/useTodos.js
+++ src/App/useTodos.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { useLocalStorage } from './useLocalStorage';
 
-const TodoContext = React.createContext();
 
-function TodoProvider(props) 
+function useTodos() 
 {
 
     const {
@@ -59,7 +58,7 @@ function TodoProvider(props)
     }
 
     return (
-        <TodoContext.Provider value={{
+        {
             loading,
             error,
             totalTodos,
@@ -72,11 +71,8 @@ function TodoProvider(props)
             deleteTodo,
             openModal,
             setOpenModal
-        }}>
-            {props.children}
-        </TodoContext.Provider>
+        } 
     );
 }
 
-export { TodoContext,
-            TodoProvider };
+export { useTodos };
