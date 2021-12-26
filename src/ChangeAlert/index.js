425e26f22f21ff2ffc25e88eb91eb98c4bb66005
Server2GJ <GermanGomezJimenez@gmail.com>
2021-12-25 22:47:11 -0500

    Clase 16: Retos

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { withStorageListener } from "./withStorageListener";
 
+import './ChangeAlert.css';
+
 
 function ChangeAlert({ show, toggleShow }) {
     if (show) {
         return (
-            <div>
-                <p>Hubo cambios</p>
-                <button
-                    onClick={() => toggleShow(false) }
-                >
-                    Volver a cargar la informacion
-                </button>
+            <div className="ChangeAlert-bg" >
+                <div className="ChangeAlert-container" >
+                    <p>Los TODOs fueron actualizados desde otro TAB o Ventana del navegador.</p>
+                    <p>¿Quieres sincronizar tus TODOs?</p>
+                    <button
+                        className="TodoForm-button TodoForm-button--add"
+                        onClick={() => toggleShow(false) }
+                    >
+                        Si !!!
+                    </button>
+                </div>
             </div>
         );
     }
